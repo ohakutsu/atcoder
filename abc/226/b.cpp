86380ba752c062ec48f73524f2089cd6c25d8977
ohakutsu <hntkaaau0842pon@yahoo.co.jp>
2021-11-07 23:05:04 +0900

    Fix b of ABC226

--- abc/226/b.cpp
+++ abc/226/b.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
-#include <string>
-#include <unordered_set>
+#include <set>
+#include <vector>
 using namespace std;
 
 int main() {
   int n;
   cin >> n;
 
-  unordered_set<string> buf;
+  set<vector<int>> buf;
 
   for (int i = 0; i < n; ++i) {
     int l;
     cin >> l;
 
-    string str = "";
-    for (int j = 0; j < l; ++j) {
-      string s;
-      cin >> s;
-
-      str += s;
+    vector<int> v(l);
+    for (int &a : v) {
+      cin >> a;
     }
 
-    if (buf.find(str) == buf.end()) {
-      buf.insert(str);
+    if (buf.find(v) == buf.end()) {
+      buf.insert(v);
     }
   }
 
